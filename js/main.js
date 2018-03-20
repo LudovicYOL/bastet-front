ae307f24a68a518119e594028a934713a938a342
YOL Ludovic <yol.l-ssii@mipih.fr>
2018-03-20 16:55:13 +0100

    Amélioration gestion login WIP

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+
+// Variable de configuration
+const baseUrl = "http://localhost/bastet-slim/v1";
+
 function initMenu() {
     $("#menu").load("components/menu.html");
     $.getScript("components/menu.js");
@@ -6,37 +10,6 @@ function initMenu() {
 function initPage(page) {
     $("#container").load("pages/" + page + ".html");
     $.getScript("pages/" + page + ".js");
-}
-
-function connect() {
-    
-    var data = {};
-    data.email = $("#email").val();
-    data.password = $("#password").val();
-
-    $.ajax({
-        type: 'POST',
-        cache: false,
-        url: 'http://localhost/bastet-slim/v1/login',
-        dataType: 'json',
-        data:data,
-        success: function(data, status, xml){
-            if (data.error) {
-                Materialize.toast(data.message, 1000);
-            } else {
-                sessionStorage.setItem('user', data.name);
-                sessionStorage.setItem('api-key', data.apiKey);
-                location.reload();
-            }
-        },
-        error: function(data, status, error){
-            Materialize.toast(data.responseJSON.message, 1000);
-        },
-        complete: function(data, status){
-            // DO NOTHING
-        }
-    });
-       
 }
 
 function disconnect() {
--- pages/login.js
+++ pages/login.js
@@ -10,3 +10,36 @@ function goToRegister(){
 function goToForgotPassword(){
     initPage("forgot-password");
 }
+
+function connect() {
+    
+    var data = {};
+    data.email = $("#email").val();
+    data.password = $("#password").val();
+
+    $.ajax({
+        type: 'POST',
+        cache: false,
+        url: baseUrl +'/login',
+        dataType: 'json',
+        data:data,
+        success: function(data, status, xml){
+            if (data.error) {
+                Materialize.toast(data.message, 1000);
+                $("#check_email").addClass("invalid");
+                $("#check_email").prop("aria-invalid", "true");
+            } else {
+                sessionStorage.setItem('user', data.name);
+                sessionStorage.setItem('api-key', data.apiKey);
+                location.reload();
+            }
+        },
+        error: function(data, status, error){
+            Materialize.toast(data.responseJSON.message, 1000);
+        },
+        complete: function(data, status){
+            // DO NOTHING
+        }
+    });
+       
+}
